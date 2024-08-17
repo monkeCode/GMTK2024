1454bf4db9517172321ab09613c6430f8ac37f8d
Сергей Бирюков <birukovsergej121@gmail.com>
2024-08-17 17:40:59 +0500

    Add simple pause menu

---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class PauseMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject pauseMenu;
+
+        private void Update()
+        {
+            HandleInput();
+        }
+
+        public void ToMainMenu()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.MainMenu);
+        }
+
+        public void Open()
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+
+        public void Close()
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+
+        private void HandleInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (pauseMenu.activeSelf)
+                {
+                    Close();
+                }
+                else
+                {
+                    Open();
+                }
+            }
+        }
+    }
+}
