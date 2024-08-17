b2e25de52a80bd95f23771f2051c08e5631e279f
Сергей Бирюков <birukovsergej121@gmail.com>
2024-08-17 15:36:22 +0500

    add resource manager

---
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Resources
+{
+    public class ResourceManager
+    {
+        public int MoneyCount { get; private set; }
+        public int FoodCount { get; private set; }
+        public int ArmyCount { get; private set; }
+
+        public void AddMoney(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Value can't be < 0");
+            }
+            
+            MoneyCount += count;
+        }
+
+        public bool TrySpendMoney(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Value can't be < 0");
+            }
+
+            if (MoneyCount < count)
+            {
+                return false;
+            }
+
+            MoneyCount -= count;
+            return true;
+        }
+
+        public void AddFood(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Value can't be < 0");
+            }
+            
+            FoodCount += count;
+        }
+        
+        public bool TrySpendFood(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Value can't be < 0");
+            }
+
+            if (FoodCount < count)
+            {
+                return false;
+            }
+
+            FoodCount -= count;
+            return true;
+        }
+
+        public void AddArmy(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Value can't be < 0");
+            }
+            
+            ArmyCount += count;
+        }
+        
+        public bool TrySpendArmy(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Value can't be < 0");
+            }
+
+            if (ArmyCount < count)
+            {
+                return false;
+            }
+
+            ArmyCount -= count;
+            return true;
+        }
+    }
+}
