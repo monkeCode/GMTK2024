8e90df560b5c2e15f8659c8bab3820b2d0430ff5
Georgy <georgy.tertychniy@yandex.ru>
2024-08-17 18:38:59 +0300

    merge confict

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,6 +7,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Generator _generator;
 
     [SerializeField] private Mapdrawer _mapDrawer;
+
+    [SerializeField] private GameObject testHome;
 
     public static GameManager Instance { get; private set; }
 
@@ -25,9 +28,20 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Manager start");
         Map = _generator.GenerateMap();
         _mapDrawer.Draw(Map);
+        _mapDrawer.DrawHouse(testHome, 0,0);
+        _mapDrawer.DrawHouse(testHome, 0,1);
+        _mapDrawer.DrawHouse(testHome, 1,1);
+        _mapDrawer.DrawHouse(testHome, 1,5);
+        _mapDrawer.DrawRoad( new List<Vector2Int>
+        {
+            new Vector2Int(1,2),
+            new Vector2Int(2,2),
+            new Vector2Int(3,2),
+            new Vector2Int(3,3),
+
+        });
     }
 
     void Update()
--- Assets/Scripts/Map drawer.cs
+++ Assets/Scripts/Map drawer.cs
@@ -1,16 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(AudioSource))]
 public class Mapdrawer : MonoBehaviour
 {
+    [Header("Tiles")]
     [SerializeField] private TileBase _ground;
     [SerializeField] private TileBase _river;
     [SerializeField] private TileBase _road;
     [SerializeField] private Tilemap _tilemap;
+
+    [Header("Map decorations")]
+    [SerializeField] private List<Sprite> _spriteDecorations;
+    [SerializeField] private GameObject _prefabDecoration;
+    [SerializeField] [Range(0,1)] private float _decorationProbability = 0.1f;
+    [SerializeField] private LayerMask _decorationLayer;
+
+    [Header("House")]
+    [SerializeField] private float _houseAnimationDuration = 0.3f;
+    [SerializeField] private AudioClip _houseAnimSound;
+
+
+    private AudioSource _audioSource;
 
     public void Draw(CellType[,] logicMap)
     {
@@ -24,7 +41,8 @@ public class Mapdrawer : MonoBehaviour
                 _ => _ground
             };
 
-            //int z = (logicMap[i, j] == CellType.River)? 999:j+i;
+            if (logicMap[i, j] == CellType.Empty)
+                SpawnDecorations(i,j);
 
             _tilemap.SetTile(new Vector3Int(i,j, 0), tile);
 
@@ -33,14 +51,75 @@ public class Mapdrawer : MonoBehaviour
         }
     }
 
-    void Start()
+    private (float,float) ConvertCoord(float x, float y)
     {
-        //var m = new CellType[5, 5];
-        //for (int i = 0; i < m.GetLength(0); i++)
-        //{
-         //   m[1, i] = CellType.River;
-        //}
-        //Draw(m);
+        var angle = Mathf.PI / 4;
+
+        var xTransformed = (x * Mathf.Cos(angle) - y * Mathf.Sin(angle))* 0.707f;
+        var yTransformed = (x * Mathf.Sin(angle) + y * Mathf.Cos(angle))*0.353f + 0.5f;
+        return (xTransformed, yTransformed);
+    }
+
+    private void SpawnDecorations(int x, int y)
+    {
+        if(Random.value > _decorationProbability)
+            return;
+
+        var xn = x + (Random.value-0.5f)/10f;
+        var yn = y +(Random.value-0.5f)/10f;
+
+        var (xTransformed, yTransformed) = ConvertCoord(xn, yn);
+
+        var go =Instantiate(_prefabDecoration, new Vector3(xTransformed,yTransformed,0), Quaternion.identity);
+        go.GetComponent<SpriteRenderer>().sprite = _spriteDecorations[Random.Range(0, _spriteDecorations.Count)];
+    }
+
+    private void DeleteDecorationAt(int x, int y)
+    {
+        var (xn, yn) = ConvertCoord(x, y);
+        foreach (var dec in Physics2D.OverlapCircleAll(new Vector2(xn, yn), 1, _decorationLayer))
+        {
+            Destroy(dec.gameObject);
+        }
+    }
+
+    private IEnumerator SpawnHouseAnimation(Transform house, float endY)
+    {
+        float startTime = Time.time;
+        float curTime = startTime;
+        float startPos = house.transform.position.y;
+        do
+        {
+            curTime = Time.time;
+            var pos = house.position;
+            pos.y = Mathf.Max((curTime - startTime) / _houseAnimationDuration * (endY - startPos) + startPos, endY);
+            house.position = pos;
+            yield return null;
+        } while (curTime - startTime <= _houseAnimationDuration);
+        _audioSource.Stop();
+        _audioSource.clip = _houseAnimSound;
+        _audioSource.Play();
+    }
+
+    public void DrawHouse(GameObject housePrefab, int x, int y)
+    {
+            var (xn, yn) = ConvertCoord(x, y);
+            DeleteDecorationAt(x,y);
+            var house = Instantiate(housePrefab, new Vector2(xn, yn+15), Quaternion.identity);
+            StartCoroutine(SpawnHouseAnimation(house.transform, yn));
+    }
+
+    public void DrawRoad(ICollection<Vector2Int> roadPoints)
+    {
+        foreach (var point in roadPoints)
+        {
+            _tilemap.SetTile((Vector3Int)point, _road);
+        }
+    }
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
     }
 }
 
